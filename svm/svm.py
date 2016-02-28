d071e0a70c169f37fc232ddebe5297a3aa056a3b
AhmedElsagher <ahmed0elsagher@gmail.com>
2016-02-28 21:29:03 +0200

    the accuracy of the of the SVM by using iris data dataSet  0.993333 using kernel =rbf and C =1000

--- svm/svm.py
+++ svm/svm.py
@@ -3,7 +3,7 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 
 iris = load_iris()
-clf = svm.SVC(kernel='rbf',C=100)
+clf = svm.SVC(kernel='rbf',C=1000)
 clf.fit(iris.data, iris.target) 
 
 
