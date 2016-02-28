da284adfa0b3bbcac2351167acca6412900f3081
AhmedElsagher <ahmed0elsagher@gmail.com>
2016-02-28 21:23:28 +0200

    the accuracy of the of the SVM by using iris data dataSet  0.986667 using kernel rbf

--- svm/svm.py
+++ svm/svm.py
@@ -3,7 +3,7 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 
 iris = load_iris()
-clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf')
 clf.fit(iris.data, iris.target) 
 
 
@@ -12,4 +12,4 @@ we will use the training dataSet as test dataSet
 '''
 y_pred =clf.predict(iris.data)
 accuracy =  accuracy_score(iris.target ,y_pred)
-print("the accuracy of the of the naive bayes by using iris data dataSet  %f" % (accuracy))
+print("the accuracy of the of the SVM by using iris data dataSet  %f" % (accuracy))
